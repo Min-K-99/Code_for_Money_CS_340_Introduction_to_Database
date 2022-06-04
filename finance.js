35e04d37de5cfbd564a106201c01f384e0a6d049
Min Khant Aung <minkhant@10-197-150-95.sds.oregonstate.edu>
2022-06-03 21:40:55 -0700

    contact.js, marketing.js, finance.js included

---
+++ finance.js
@@ -0,0 +1,28 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
+    function getContact (res, mysql, context, complete) {
+        let results = mysql.pool.query("SELECT * FROM finance", function(error, results, fields) {
+            if(error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.finance = results;
+            complete();
+        });
+    }
+
+    router.get('/', function(req, res) {
+        var callbackCount = 0;
+        var context = {};
+        // context.jsscripts = ["delete.js", "filter.js", "search.js"];
+        var mysql = req.app.get('mysql');
+        getContact(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            res.render('finance', {"tableData": context.finance});
+        }
+    });
+    return router;
+} ();
